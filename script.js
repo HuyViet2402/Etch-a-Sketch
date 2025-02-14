56408216b292bba048b7156547d648dae9feb527
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-02-14 13:16:13 +0000

    Change grid style in css

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function generateGrid(n) {
         div_row.setAttribute("style", "display: flex;")
         for (let j = 0; j < n; j++) {
             const div_square = document.createElement("div");
-            div_square.setAttribute("style", "background-color: white; border-style: solid; width: 30px; height: 30px; margin: 0.5px;");
+            div_square.setAttribute("style", "background-color: white; width: 30px; height: 30px;");
             div_square.addEventListener("mouseenter", () => {
                 let color = inputColor.value;
-                div_square.setAttribute("style", "background-color:" + color.toString() + "; border-style: solid; width: 30px; height: 30px; margin: 0.5px;");
+                div_square.setAttribute("style", "background-color:" + color.toString() + "; width: 30px; height: 30px;");
             })
             div_row.append(div_square);
         }
